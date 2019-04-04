49940d0f61ef335e8f8aba41d0b41619be354ce0
Arron M <marshall.arron@gmail.com>
2019-04-04 10:06:35 -0700

    Finish prototype-refactor assignment

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -1,22 +1,3 @@
-/* 
-
-Prototype Refactor
-
-1. Copy and paste your code or the solution from yesterday
-
-2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
-
-*/
-/*
-  Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance hierarchy.
-
-  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.  
-
-  At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
-  
-  Each constructor function has unique properties and methods that are defined in their block comments below:
-*/
-  
 /*
   === GameObject ===
   * createdAt
@@ -25,14 +6,16 @@ Prototype Refactor
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
 
-function GameObject (props) {
-  this.createdAt = props.createdAt;
-  this.name = props.name;
-  this.dimensions = props.dimensions;
-}
+class GameObject {
+  constructor (props) {
+    this.createdAt = props.createdAt;
+    this.name = props.name;
+    this.dimensions = props.dimensions;
+  }
 
-GameObject.prototype.destroy = function () {
-  return `${this.name} was removed from the game.`;
+  destroy() {
+    return `${this.name} was removed from the game`;
+  }
 }
 
 /*
@@ -42,15 +25,16 @@ GameObject.prototype.destroy = function () {
   * should inherit destroy() from GameObject's prototype
 */
 
-function CharacterStats (props) {
-  GameObject.call(this, props);
-  this.healthPoints = props.healthPoints;
-}
-CharacterStats.prototype = Object.create(GameObject.prototype);
+class CharacterStats extends GameObject {
+  constructor (props) {
+    super(props);
+    this.healthPoints = props.healthPoints;
+  }
 
-CharacterStats.prototype.takeDamage = function (damage) {
-  this.healthPoints = this.healthPoints - damage;
-  return `${this.name} took damage.`;
+  takeDamage(damage) {
+    this.healthPoints = this.healthPoints - damage;
+    return `${this.name} took ${damage} damage`;
+  }
 }
 
 /*
@@ -61,18 +45,19 @@ CharacterStats.prototype.takeDamage = function (damage) {
   * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
-*/
+  */
 
-function Humanoid (props) {
-  CharacterStats.call(this, props);
-  this.team = props.team;
-  this.weapons = props.weapons;
-  this.language = props.language;
-}
-Humanoid.prototype = Object.create(CharacterStats.prototype);
+class Humanoid extends CharacterStats {
+  constructor (props) {
+    super(props);
+    this.team = props.team;
+    this.weapons = props.weapons;
+    this.language = props.language;
+  }
 
-Humanoid.prototype.greet = function () {
-  return `${this.name} offers a greeting in ${this.language}`;
+  greet() {
+    return `${this.name} offers a greeting in ${this.language}`;
+  }
 }
  
 /*
@@ -145,44 +130,27 @@ Humanoid.prototype.greet = function () {
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
 
   // Stretch task: 
-  // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
-  function Villain (props) {
-    Humanoid.call(this, props);
-  }
-  Villain.prototype = Object.assign(Humanoid.prototype);
-  
-  function Hero (props) {
-    Humanoid.call(this, props);
-  }
-  Hero.prototype = Object.assign(Humanoid.prototype);
+  // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.
 
-  // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
-  // NOTE: Should probably create an 'Attack' constructor or object property and then assign new Attacks to the hero and villain, but pair programming has left me no time
-  Villain.prototype.evilBeam = function (target) {
-    this.target = target;
-    this.damage = Math.floor(Math.random() * 10);
-    target.takeDamage(this.damage);
-    if (target.healthPoints <= 0) {
-      target.destroy();
-      return `${this.name} has won the battle!`;
+  class Antagonist extends Humanoid {
+    constructor (props) {
+      super(props);
     }
-    return `${this.name} hit ${this.target.name} for ${this.damage}`;
-  };
 
-  Hero.prototype.acidBreath = function (target) {
-    this.target = target;
-    this.damage = Math.floor(Math.random() * 10);
-    target.takeDamage(this.damage);
-    if (target.healthPoints <= 0) {
-      target.destroy();
-      return `${this.name} has won the battle!`;
+    attack(target) {
+      const damage = Math.floor(Math.random() * 10);
+      target.takeDamage(damage);
+      if (target.healthPoints <= 0) {
+        target.destroy();
+        return `${this.name} has won the battle!`;
+      }
+      return `${this.name} hit ${target.name} for ${damage}`
     }
-    return `${this.name} hit ${this.target.name} for ${this.damage}`;
-  };
+  }
 
   // * Create two new objects, one a villain and one a hero and fight it out with methods!
 
-  let Trok = new Hero({
+  let Trok = new Antagonist({
     createdAt: new Date(),
     dimensions: {
       length: 2,
@@ -199,14 +167,14 @@ Humanoid.prototype.greet = function () {
     language: 'Draconic',
   });
 
-  const Verc = new Villain({
+  const Verc = new Antagonist({
     createdAt: new Date(),
     dimensions: {
       length: 1,
       width: 2,
       height: 5,
     },
-    healthPoints: 20, /*?*/
+    healthPoints: 20,
     name: 'Verc',
     team: 'Bad Guys',
     weapons: [
@@ -216,11 +184,10 @@ Humanoid.prototype.greet = function () {
     language: 'Common',
   });
 
-  // TODO: implement while loop until there is a winner
   while (Trok.healthPoints > 0 && Verc.healthPoints > 0) {
-    if (Trok.acidBreath(Verc).match('won the battle!')) {
+    if (Trok.attack(Verc).match('won')) {
       console.log('Trok has valiantly won the battle!');
-    } else if (Verc.evilBeam(Trok).match('won the battle!')) {
+    } else if (Verc.attack(Trok).match('won')) {
       console.log('Oh no! Our hero, Trok, has been defeated by the evil villain Verc! Who will protect us now, take cover in your homes!');
     }
     console.log(`Trok's health: ${Trok.healthPoints}, Verc's health: ${Verc.healthPoints}`);
